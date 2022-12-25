0389b4dbd218906cdd9a052fe45a7d94f004aaab
BogusKladik <boguskladik@gmail.com>
2022-12-25 15:15:48 +0300

    refactor and added new function

--- 4/accuracy.py
+++ 4/accuracy.py
@@ -8,5 +8,20 @@ def accuracy(function_integral, expect, percent):
         n *= 2
         current = function_integral(n)
         current_percent = abs((expect - current) / expect)
-    
+
+    return current, n
+
+
+def accuracy_mc(function_integral, percent):
+    current_percent = 1
+    n = 1
+    prev = abs(function_integral(n))
+
+    current = 0
+    while current_percent > percent:
+        n *= 2
+        current = abs(function_integral(n))
+        current_percent = abs(current - prev)
+        prev = current
+
     return current, n
--- 4/condition.py
+++ 4/condition.py
@@ -2,24 +2,17 @@ import math
 import numpy as np
 
 
-# def function():
-#     return lambda x: x**2
-
-
-# def interval():
-#     return (0.3, 3.0)
-
 def function():
-    return lambda x: np.cos(x)
+    return lambda x: x**2
+
 
 def interval():
-    return (-1.0, 1.5)
+    return (0.3, 3.0)
 
-# def solution():
-#     return 8.991
 
 def solution():
-    return 1.83897
+    return 8.991
+
 
 def print_solution():
     print("Первообразная от x^2 = x^3/3\nОпределенный интеграл в интервале от 0.3 до 3 = 8.991")
